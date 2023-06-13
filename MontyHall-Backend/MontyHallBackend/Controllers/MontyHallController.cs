549ae5e3ea1547c40d9e75eeb69de7068af98c53
Supun Laksitha <73895628+LaksithaKS@users.noreply.github.com>
2023-06-13 13:10:24 +0530

    Add files via upload

---
+++ MontyHall-Backend/MontyHallBackend/Controllers/MontyHallController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MontyHallBackend.Models;
+
+namespace MontyHall_Backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MontyHallController : Controller
+	{
+		[HttpPost("result")]
+		public IActionResult Index(MontyHall montyHall)
+		{
+			{
+				int totalWins = 0;
+				int totalLosts = 0;
+
+				Random random = new Random(); //generate random number
+
+				for (int i = 0; i < montyHall.numberOfGames; i++)
+				{
+					int winningDoor = random.Next(1, 4);
+					int initialChoice = random.Next(1, 4);
+
+					if (montyHall.changeTheDoor)
+					{
+						if (initialChoice == winningDoor)
+							totalLosts++;
+						else
+							totalWins++;
+					}
+					else
+					{
+						if (initialChoice == winningDoor)
+							totalWins++;
+						else
+							totalLosts++;
+					}
+				}
+
+				var result = new
+				{
+					winCount = totalWins,
+					total = montyHall.numberOfGames
+				};
+
+				return Ok(result);
+			}
+		}
+	}
+}
---
+++ MontyHall-Backend/MontyHallBackend/Models/MontyHall.cs
@@ -0,0 +1,8 @@
+﻿namespace MontyHallBackend.Models
+{
+	public class MontyHall
+	{
+		public int numberOfGames { get; set; }
+		public bool changeTheDoor { get; set; }
+	}
+}
